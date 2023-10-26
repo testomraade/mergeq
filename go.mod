93bab995dc30d4a29da12549f7977e052f7f5110
Birol Bilgin <birolbilgin@gmail.com>
2023-10-26 12:15:14 +0200

    first commit
    Signed-off-by: Birol Bilgin <birolbilgin@gmail.com>

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/isovalent/mergeq
+
+go 1.21.3
+
+require (
+	github.com/digineo/go-ping v1.0.1
+	github.com/fatih/color v1.15.0
+	inet.af/netaddr v0.0.0-20230525184311-b8eac61e914a
+)
+
+require (
+	github.com/digineo/go-logwrap v0.0.0-20181106161722-a178c58ea3f0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20230525183740-e7c30c78aeb2 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/digineo/go-ping"
+	"github.com/fatih/color"
+	"inet.af/netaddr"
+)
+
+var timeout = flag.Duration("timeout", time.Second, "ping timeout")
+
+func main() {
+	flag.Parse()
+
+	nets := flag.Args()
+	if len(nets) == 0 {
+		fmt.Fprintln(os.Stderr, "at least one network/ip is required")
+		os.Exit(1)
+	}
+
+	ips := map[netaddr.IP]struct{}{}
+
+	for _, n := range nets {
+		ip, err := netaddr.ParseIP(n)
+		if err == nil {
+			ips[ip] = struct{}{}
+			continue
+		}
+
+		ipr, err := netaddr.ParseIPRange(n)
+		if err == nil {
+			for ip := ipr.From(); ip.Compare(ipr.To()) <= 0; ip = ip.Next() {
+				ips[ip] = struct{}{}
+			}
+			continue
+		}
+
+		ipp, err := netaddr.ParseIPPrefix(n)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "network/ip %s is not a valid ip, ip range or network\n", n)
+			os.Exit(1)
+		}
+
+		ipr = ipp.Range()
+		for ip := ipr.From(); ip.Compare(ipr.To()) <= 0; ip = ip.Next() {
+			ips[ip] = struct{}{}
+		}
+	}
+
+	p, err := ping.New("0.0.0.0", "::")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// for ip := range ips {
+	// 	d, err := p.Ping(ip.IPAddr(), time.Second)
+	// 	fmt.Println(ip, d, err)
+	// }
+
+	ipc := make(chan netaddr.IP, 10)
+	wg := sync.WaitGroup{}
+	writeLock := sync.Mutex{}
+
+	go func() {
+		for ip := range ipc {
+			go func(ip netaddr.IP) {
+				// fmt.Println(ip)
+				_, err := p.Ping(ip.IPAddr(), *timeout)
+
+				writeLock.Lock()
+				fmt.Print(ip, "\t")
+				if err == nil {
+					color.Green("UP")
+				} else {
+					color.Red("DOWN")
+				}
+				writeLock.Unlock()
+
+				wg.Done()
+			}(ip)
+		}
+	}()
+
+	for ip := range ips {
+		// select {
+		// case ipc <- ip:
+		// default:
+		// 	continue
+		// }
+		ipc <- ip
+		wg.Add(1)
+	}
+
+	wg.Wait()
+}
