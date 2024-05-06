4d7df7f04a197be8bb7805f5d258eb0543d82204
Birol Bilgin <birolbilgin@gmail.com>
2024-05-06 14:05:24 +0200

    replaced netaddr pkg
    replaced netaddr package with netip

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
+	"net/netip"
 	"os"
 	"sync"
 	"time"
 
 	"github.com/digineo/go-ping"
 	"github.com/fatih/color"
-	"inet.af/netaddr"
 )
 
 var timeout = flag.Duration("timeout", time.Second, "ping timeout")
@@ -23,31 +24,31 @@ func main() {
 		os.Exit(1)
 	}
 
-	ips := map[netaddr.IP]struct{}{}
+	ips := map[netip.Addr]struct{}{}
 
 	for _, n := range nets {
-		ip, err := netaddr.ParseIP(n)
+		ip, err := netip.ParseAddr(n)
 		if err == nil {
 			ips[ip] = struct{}{}
 			continue
 		}
 
-		ipr, err := netaddr.ParseIPRange(n)
+		ipr, err := parseIPRange(n)
 		if err == nil {
-			for ip := ipr.From(); ip.Compare(ipr.To()) <= 0; ip = ip.Next() {
+			for ip := ipr.from; ip.Compare(ipr.to) <= 0; ip = ip.Next() {
 				ips[ip] = struct{}{}
 			}
 			continue
 		}
 
-		ipp, err := netaddr.ParseIPPrefix(n)
+		ipp, err := netip.ParsePrefix(n)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "network/ip %s is not a valid ip, ip range or network\n", n)
 			os.Exit(1)
 		}
 
-		ipr = ipp.Range()
-		for ip := ipr.From(); ip.Compare(ipr.To()) <= 0; ip = ip.Next() {
+		ipp = ipp.Masked()
+		for ip := ipp.Addr(); ipp.Contains(ip); ip = ip.Next() {
 			ips[ip] = struct{}{}
 		}
 	}
@@ -58,20 +59,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	// for ip := range ips {
-	// 	d, err := p.Ping(ip.IPAddr(), time.Second)
-	// 	fmt.Println(ip, d, err)
-	// }
-
-	ipc := make(chan netaddr.IP, 10)
+	ipc := make(chan netip.Addr, 10)
 	wg := sync.WaitGroup{}
 	writeLock := sync.Mutex{}
 
 	go func() {
 		for ip := range ipc {
-			go func(ip netaddr.IP) {
-				// fmt.Println(ip)
-				_, err := p.Ping(ip.IPAddr(), *timeout)
+			go func(ip netip.Addr) {
+				_, err := p.Ping(&net.IPAddr{IP: ip.AsSlice()}, *timeout)
 
 				writeLock.Lock()
 				fmt.Print(ip, "\t")
@@ -88,11 +83,6 @@ func main() {
 	}()
 
 	for ip := range ips {
-		// select {
-		// case ipc <- ip:
-		// default:
-		// 	continue
-		// }
 		ipc <- ip
 		wg.Add(1)
 	}
