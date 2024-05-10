8eb65d3e01ef7997534af62d7fc70548aabf1e31
Birol Bilgin <birolbilgin@gmail.com>
2024-05-10 12:23:25 +0200

    added tcp ping
    the default port is 22

--- ping.go
+++ ping.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/netip"
 	"sync"
@@ -20,6 +21,23 @@ func runIcmpPing(p *ping.Pinger, wgi, wgp *sync.WaitGroup, ipc chan netip.Addr, 
 		for ip := range ipc {
 			go func(ip netip.Addr) {
 				_, err := p.Ping(&net.IPAddr{IP: ip.AsSlice()}, *timeout)
+				wgp.Add(1)
+				rec <- result{ip: ip, err: err}
+				wgi.Done()
+			}(ip)
+		}
+	}()
+}
+
+// runTcpPing runs tcp pings
+func runTcpPing(port int, wgi, wgp *sync.WaitGroup, ipc chan netip.Addr, rec chan result) {
+	go func() {
+		for ip := range ipc {
+			go func(ip netip.Addr) {
+				conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip, port), *timeout)
+				if err == nil {
+					defer conn.Close()
+				}
 				wgp.Add(1)
 				rec <- result{ip: ip, err: err}
 				wgi.Done()
